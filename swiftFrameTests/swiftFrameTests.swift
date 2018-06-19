e21ceff6579c0848c935f52454d0b9d808708504
apple <apple@appledemacbook-pro.local>
2018-06-19 15:31:37 +0800

    Initial Commit

---
+++ swiftFrameTests/swiftFrameTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftFrameTests.swift
+//  swiftFrameTests
+//
+//  Created by apple on 2018/6/19.
+//  Copyright © 2018年 wz. All rights reserved.
+//
+
+import XCTest
+@testable import swiftFrame
+
+class swiftFrameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
